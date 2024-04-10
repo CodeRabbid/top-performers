b82ef2a513178408e3a2e69ffb307faaf5025689
Artiyom <artiyom.moskalew.2@gmail.com>
2024-04-10 16:46:38 +0200

    serve static site from backend

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 const port = 5000;
 
@@ -8,5 +9,14 @@ app.use(express.json());
 app.get('/api/hello', (req, res)=>{
   res.json({message: "Top Performers"})
 });
+
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res) =>
+  res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+);
+
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   useEffect(()=>{
     (async () => {
-      const response = await fetch('https://upward-sales.onrender.com/api/hello',
+      const response = await fetch('/api/hello',
       {
         headers : { 
         'Content-Type': 'application/json',
