efa9d2a58d09c7591b8a510b9115d59a4568ae1e
Artiyom <artiyom.moskalew.2@gmail.com>
2024-04-14 14:47:55 +0200

    formatting

--- cypress/e2e/3-top-performers/header.cy.js
+++ cypress/e2e/3-top-performers/header.cy.js
@@ -1,14 +1,11 @@
 /// <reference types="cypress" />
 
-describe('visit top performers', () => {
+describe("visit top performers", () => {
   beforeEach(() => {
-    cy.visit('http://localhost:5000');
-  })
+    cy.visit("http://localhost:5000");
+  });
 
-  it('displays title', () => {
-    cy.get('#logo').should('have.text', 'Top Performers')
-  })
-})
-
-
-
+  it("displays title", () => {
+    cy.get("#logo").should("have.text", "Top Performers");
+  });
+});
