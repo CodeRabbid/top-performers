83e281e9570c90ef20db61140ca01063f1388638
Artiyom <artiyom.moskalew.2@gmail.com>
2024-04-19 21:09:30 +0200

    move AuthProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,6 +13,7 @@ import RegisterScreen from "./screens/RegisterScreen.jsx";
 import store from "./store";
 import LoginScreen from "./screens/LoginScreen.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import AuthProvider from "./components/AuthProvider";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -25,14 +26,16 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <GoogleOAuthProvider
-      clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}
-      plugin_name="Memories App"
-      cookiepolicy="single_host_origin"
-    >
-      <React.StrictMode>
-        <RouterProvider router={router} />
-      </React.StrictMode>
-    </GoogleOAuthProvider>
+    <AuthProvider>
+      <GoogleOAuthProvider
+        clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}
+        plugin_name="Memories App"
+        cookiepolicy="single_host_origin"
+      >
+        <React.StrictMode>
+          <RouterProvider router={router} />
+        </React.StrictMode>
+      </GoogleOAuthProvider>
+    </AuthProvider>
   </Provider>
 );
