c0a008c6d8b79d4188e3bd0a7e819cefa007c5ed
Artiyom <artiyom.moskalew.2@gmail.com>
2024-05-12 16:40:24 +0200

    change styling to display two diagrams

--- frontend/src/screens/DiagramsScreen.jsx
+++ frontend/src/screens/DiagramsScreen.jsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { BarChart } from "@mui/x-charts/BarChart";
 import { axisClasses } from "@mui/x-charts";
 import { useGetDiagramMutation } from "../slices/api/itemsApiSlice";
 import DiagramFilters from "../components/DiagramFilters";
 
 import dayjs from "dayjs";
+import { backdropClasses } from "@mui/material";
 
 const chartSetting = {
   yAxis: [
@@ -68,25 +69,73 @@ const DiagramsScreen = () => {
       ? `${value} items`
       : `${formatter.format(value)}`;
   return (
-    <div
-      style={{
-        height: "100%",
-        flex: "1 1 auto",
-        pointerEvents: isLoading ? "none" : "",
-      }}
-    >
-      <BarChart
-        dataset={diagramData.data}
-        xAxis={[{ scaleType: "band", dataKey: "time_unit" }]}
-        series={diagramData.series}
-        {...chartSetting}
-      />
-
-      <DiagramFilters
-        selectedFilters={selectedFilters}
-        setSelectedFilters={setSelectedFilters}
-        handleApply={handleApply}
-      />
+    // <div
+    //   style={{
+    //     height: "100%",
+    //     flex: "1 1 auto",
+    //     pointerEvents: isLoading ? "none" : "",
+    //   }}
+    // >
+    <div id="filtered-table">
+      <div
+        style={{
+          flex: "1 1 auto",
+          height: "0%",
+          overflowY: "scroll",
+        }}
+      >
+        <div
+          style={{
+            display: "block",
+            margin: "0px auto  0px auto",
+            position: "relative",
+            width: "500px",
+          }}
+          key={0}
+        >
+          <BarChart
+            margin={{
+              left: 70,
+              right: 20,
+              top: 20,
+              bottom: 30,
+            }}
+            dataset={diagramData.data}
+            xAxis={[{ scaleType: "band", dataKey: "time_unit" }]}
+            series={diagramData.series}
+            {...chartSetting}
+          />
+        </div>
+        <div
+          style={{
+            display: "block",
+            margin: "0px auto  0px auto",
+            position: "relative",
+            width: "500px",
+          }}
+          key={1}
+        >
+          <BarChart
+            margin={{
+              left: 70,
+              right: 20,
+              top: 20,
+              bottom: 30,
+            }}
+            dataset={diagramData.data}
+            xAxis={[{ scaleType: "band", dataKey: "time_unit" }]}
+            series={diagramData.series}
+            {...chartSetting}
+          />
+        </div>
+      </div>
+      <div style={{}}>
+        <DiagramFilters
+          selectedFilters={selectedFilters}
+          setSelectedFilters={setSelectedFilters}
+          handleApply={handleApply}
+        />
+      </div>
     </div>
   );
 };
